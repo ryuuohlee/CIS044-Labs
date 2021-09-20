9d485a90d5075ca21498d4cf59259d5e294b7877
Jeffrey Lee <jglee1@live.com>
2021-09-19 22:47:15 -0700

    Feature: started lab 3

--- Lab-2/Minimum.java
+++ Lab-2/Minimum.java
@@ -2,4 +2,16 @@
 Write a generic method called minimum that accepts four items and find the minium of them.
 
 Test your class with Integer, Double, Character, String, and Car objects.
- */
+*/
+
+public interface Minimum<T> {
+    public static <T> void minimum(T[] array) {
+        T min = array[0];
+
+        for(T element: array) {
+            if(element < min) {
+                min = element;
+            }
+        }
+    }
+}
